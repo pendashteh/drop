983a925c37dec587c3be19f6717e583c94441cc6
Alexar <hi@alexar.me>
2016-02-11 15:37:22 +1100

    Fix install command minor bugs and re-structring the bash file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Usage: ./install.sh [-y]
+# Usage: $ drop -- install.sh [-y]
 set -e
 
 [[ $1 = "-y" ]] && force_yes="-y"
@@ -14,50 +14,79 @@ set -e
 # 4. We just import 'db dump'
 #	si minimal; sqlc; rr;
 
+main() {
+
+	_check_database_connection
+
+	_generate_settings_php $config_install_db_url
+
+	if [[ -e $config_install_db_dump ]]
+		then
+		echo "Re-installing "$config_profile_name" on top of DB dump."
+		_import_db $config_install_db_dump
+		enable_profile $config_profile_name
+
+	else
+
+		if [ ! "$config_install_base_profile" ] || [ "$config_install_base_profile" = "$config_profile_name" ]
+			then
+			_install_profile $config_profile_name
+		else
+			_install_profile $config_install_base_profile
+			enable_profile $config_profile_name
+		fi
+	fi
+
+
+	drush --root=$config_build_path updb $force_yes
+
+	drush --root=$config_build_path cc all
+
+	if [ "$config_install_features_revert_all" = "true" ]
+		then
+		drush --root=$config_build_path fra $force_yes
+	fi
+
+	if [ "$config_install_print_uli" = "true" ]
+		then
+		drush --root=$config_build_path uli --browser=0
+	fi
+
+	if [ -s "$config_install_post_script" ]
+		then
+		_exec_script $config_install_post_script;
+	fi
+}
+
 _check_database_connection() {
+	# @FIXME this method will not detect wrong port and will pass anyways.
 	mysql_command=$(drush --root=$config_build_path sql-connect --db-url=$config_install_db_url)
 	error="$($mysql_command -e ';')"
 }
-
-_check_database_connection
 
 enable_profile() {
 	drush --root=$config_build_path vset --exact -y install_profile $1
 	drush --root=$config_build_path en $1 $force_yes
 }
 
-echo "Installation of "$config_profile_name" on top of "$config_install_base_profile
+_generate_settings_php() {
+	local __db_url=$1
 
-drush --root=$config_build_path si $config_install_base_profile --db-url=$config_install_db_url $force_yes
+	drush --root=$config_build_path dl settingsphp $force_yes
+	drush --root=$config_build_path cc drush $force_yes
+	drush --root=$config_build_path settingsphp-generate --db-url=$__db_url $force_yes
+}
 
-if [[ -e $config_install_db_dump ]]
-	then
-	echo "Re-installing "$config_profile_name" on top of DB dump."
+_install_profile() {
+	local __profile=$1
+	drush --root=$config_build_path si $force_yes
+}
 
-	drush --root=$config_build_path si minimal --db-url=$config_install_db_url $force_yes
-	drush --root=$config_build_path sql-cli < $config_install_db_dump
-
+_import_db() {
+	local __db_dump=$1
+	drush --root=$config_build_path sql-cli < $__db_dump
 	echo "Rebuilding registry..."
 	php $script_root/scripts/rr.php --root=$config_build_path 1>/dev/null
-fi
+}
 
-enable_profile $config_profile_name
-
-drush --root=$config_build_path updb $force_yes
-
-drush --root=$config_build_path cc all
-
-if [ "$config_install_features_revert_all" = "true" ]
-	then
-	drush --root=$config_build_path fra $force_yes
-fi
-
-if [ "$config_install_print_uli" = "true" ]
-	then
-	drush --root=$config_build_path uli --browser=0
-fi
-
-if [ -e "$config_install_post_script" ]
-	then
-	_exec_script $config_install_post_script;
-fi
+main
