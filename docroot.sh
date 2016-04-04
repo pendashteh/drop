b3345f29ab10a93116b7a0e52fc1a69216d922ea
Alexar <hi@alexar.me>
2016-04-04 15:10:57 +1000

    Fixes docroot command on MacOSX.

--- docroot.sh
+++ docroot.sh
@@ -5,6 +5,6 @@ set -e
 echo "Going to $drop_docroot"
 echo "to exit type 'exit'"
 cd $drop_docroot
-init_file=$(mktemp)
+init_file=$(mktemp -t XXXX)
 echo 'PS1="drop> \$(pwd)$ "' > $init_file
 exec bash --init-file $init_file
