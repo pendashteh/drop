9892e9b7c43fab58d3feb589a26b4e339369f23c
Alexar <hi@alexar.me>
2016-04-05 10:15:35 +1000

    Adds update command.

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Usage: $ drop -- update [-y]
+set -e
+
+[[ $1 = "-y" ]] && force_yes="-y"
+
+main() {
+
+	debug drush --root=$drop_docroot updb $force_yes
+
+	debug drush --root=$drop_docroot cc all
+
+	if [ "$config_install_features_revert_all" = "true" ]
+		then
+		debug drush --root=$drop_docroot fra $force_yes
+	fi
+
+	if [ "$config_install_print_uli" = "true" ]
+		then
+		drush --root=$drop_docroot uli --browser=0 --uri=$config_drupal_url
+	fi
+
+	echo "Finished successfully."
+	exit 0
+}
+
+main
