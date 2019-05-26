64ea7196b2ab9cb4404627ea3e538033e867dbd0
Alexar <github@alexar.me>
2019-05-26 15:47:40 +1000

    Fix minor documentation typo in drushuli command.

--- drushuli.sh
+++ drushuli.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Usage: $ drop local.yml drush uli
+# Usage: $ drop local.yml drushuli
 # Runs drush in the build path
 set -e
-
 $drush --root=$drop_docroot uli --browser=0 --uri=$config_drupal_url
